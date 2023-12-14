481edade23827f162a152a048ade29ec2949620f
AndreiLiachytski <andrei.liachytski@gmail.com>
2023-12-14 12:07:39 +0300

    test commit 2

--- src/main/java/com/chitts/controller/EmployeeController.java
+++ src/main/java/com/chitts/controller/EmployeeController.java
@@ -24,6 +24,7 @@ public class EmployeeController {
     @GetMapping
     @ApiOperation(value = "Get all employees")
     public List<EmployeeShortDto> getAll() throws EmployeeDaoException {
+        // ffsfffs
         return employeeService.getAll();
     }
 
